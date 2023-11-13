c22dcd74f6f73536de95c4462a6cbab51a288e8c
Marfol_linux <marfol@inbox.ru>
2023-11-14 02:34:34 +0300

    add js in main blade

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -65,6 +65,24 @@
             modalRegister.show()
         })
 
+        // $('.modalAuth')
+
+        console.log( $( "form [type=password]" ).attr("placeholder", "Минимум 8 символов !"))
+
+        // $( ".modalAuth form" ).on( "submit", function( event ) {
+
+        //     // if ( $( "input" ).first().val() === "correct" ) {
+        //     //     $( "span" ).text( "Validated..." ).show();
+        //     // return;
+        //     // }
+
+        //     return false;
+
+            
+        //     $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
+        //     event.preventDefault();
+        // } );
+
     </script>
 
     </body>
