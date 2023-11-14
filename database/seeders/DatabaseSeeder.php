b22a7259b1bc9c6d9cd6adcb87b8aad780001e53
Marfol_linux <marfol@inbox.ru>
2023-11-14 12:09:47 +0300

    add items

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -63,7 +63,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
-        Item::factory()->count(5)->create();
+        Item::factory()->count(30)->create();
 
 
         DB::table('genres')->insert([
